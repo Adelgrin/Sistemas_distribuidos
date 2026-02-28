17c701b55b0c62fe5a92c377de9a5626e9fbec7e
Adelgrin <lamaral2002@gmail.com>
2026-02-27 21:09:13 -0300

    inicio lab reqrep

--- request-reply/src/broker/cliente.py
+++ request-reply/src/broker/cliente.py
@@ -1,15 +1,24 @@
 import zmq
 from time import sleep
+import random
 
 context = zmq.Context()
 socket = context.socket(zmq.REQ)
 socket.connect("tcp://broker:5555")
 
-i = 0
+acoes = ["adiciona", "remove"]
+tarefas = ["arrumar_cama","fazer_projeto","bom_dia","cleitin","TCC","enfim"]
+
+
 while True:
-    print(f"Mensagem {i}:", end=" ", flush=True)
-    socket.send(b"Hello")
+    # print(f"Mensagem {i}:", end=" ", flush=True)
+    # socket.send(b"Hello")
+    # mensagem = socket.recv()
+    # print(f"{mensagem}")
+    oqfazer = f"{random.sample(acoes,1)[0]} {random.sample(tarefas,1)[0]}"
+    # print(oqfazer)
+    socket.send_string(oqfazer)
     mensagem = socket.recv()
     print(f"{mensagem}")
-    i += 1
     sleep(0.5)
+
--- request-reply/src/broker/servidor.py
+++ request-reply/src/broker/servidor.py
@@ -1,11 +1,41 @@
 import zmq
 
+tarefas = []
+
 context = zmq.Context()
 socket = context.socket(zmq.REP)
 socket.connect("tcp://broker:5556")
 
+def create(tarefa):
+    tarefas.append(tarefa)
+    # socket.send_string("tarefa adicionada")
+    socket.send_string(listar())
+
+def remove(tarefa):
+    try:
+        tarefas.remove(tarefa)
+        socket.send_string("tarefa removida")
+    except ValueError as e:
+        socket.send_string(f"a tarefa {e} não existe")
+
+
+def listar():
+    payload = ""
+    for i in tarefas:
+        payload += (i + "\n")
+    print(payload)
+    # socket.send_string(payload)
+    return payload
+
 while True:
     message = socket.recv()
-    print(f"Mensagem recebida: {message}", flush=True)
-    socket.send_string("World")
-
+    # print(f"Mensagem recebida: {message}", flush=True)
+    # socket.send_string("World")
+    toprocess = str(message)
+    toprocess.split()
+    if toprocess[0].lower() == 'adiciona':
+        create(toprocess[1])
+    elif toprocess[0].lower() == 'remove':
+        remove(toprocess[1])
+    else:
+        listar()
